07df9ce89efc4f8d58bd4a8f6883db58ab099961
mima <kukuri3@gmail.com>
2015-09-04 13:33:16 +0900

    exif read function

--- exif_furiwake/Form1.cs
+++ exif_furiwake/Form1.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
 
 namespace WindowsFormsApplication1
 {
@@ -39,27 +42,57 @@ namespace WindowsFormsApplication1
 
                 foreach (string d in drags)
                 {
-                    if (System.IO.File.Exists(d)) xImgProcess(d);   //ファイルの場合
+                    if (System.IO.File.Exists(d)) xFileProcess(d);   //ファイルの場合
                     else if (System.IO.Directory.Exists(d))
                     {
                         string[] files = System.IO.Directory.GetFiles(d, "*", System.IO.SearchOption.AllDirectories);
                         foreach (string d2 in files)
                         {
-                            xImgProcess(d2);    //ディレクトリ内のファイルの場合
+                            xFileProcess(d2);    //ディレクトリ内のファイルの場合
                         }
                     }
                 }
 //                e.Effect = DragDropEffects.Copy;
             }
         }
-        private void xImgProcess(string d)
+        private void xFileProcess(string d)
         {
-            string cname = xGetCameraName(d);
-            textBox1.AppendText(d+" : "+cname + "\n");
+            string cname="null";
+            DateTime ctime=DateTime.Now;
+            int changedateflag = 0;
+
+            //拡張子を取得
+            string ext = System.IO.Path.GetExtension(d);
+            
+            //exif情報を取得
+            if (ext.Equals(".jpg") || ext.Equals(".JPG"))
+            {
+                xGetExifInfo(d, out ctime, out cname);
+                changedateflag = 1;
+            }
+            else if (ext.Equals(".gif") || ext.Equals(".GIF"))
+            {
+                cname = "gif";
+            }
+
+            xLog(d + " : " + ext + " : " + cname + " : " + ctime.ToString());
+
+            //ファイルの移動
+            string dstfn = Path.GetFileName(d);
+            string dstpathfn = System.IO.Path.Combine(cname, dstfn);
+            xLog(dstpathfn);
+
+            
+
+            //System.IO.File.Copy(d,dstpathfn);
+            FileSystem.CopyFile(d, dstpathfn,UIOption.AllDialogs, UICancelOption.DoNothing);
+            // メッセージ処理を促して表示を更新する
+            Application.DoEvents();
         }
-        private string xGetCameraName(string d)
+        private void xGetExifInfo(string d,out DateTime exiftime,out string cameraname)
         {
-            string cname = "null";
+            exiftime = DateTime.Now;
+            cameraname = "jpg";
             string imgFile = d;
 
             //読み込む
@@ -75,17 +108,33 @@ namespace WindowsFormsApplication1
                     val = val.Trim(new char[] { '\0' });
                     //表示する
                     //textBox1.AppendText("id:" + item.Id + " type:" + item.Type + " val" + val + "\n");
-                    cname = val;
+                    cameraname = val;
                 }
-                else
+                //Exif情報から撮影時間を取得する
+                if (item.Id == 0x9003 && item.Type == 2)
                 {
-                    //表示する
-                    //textBox1.AppendText("id:" + item.Id + " type:" + item.Type + " len:" + item.Len + "\n");
+                    //文字列に変換する
+                    string val = System.Text.Encoding.ASCII.GetString(item.Value);
+                    val = val.Trim(new char[] { '\0' });
+                    //DateTimeに変換
+                    DateTime dt = DateTime.ParseExact(val, "yyyy:MM:dd HH:mm:ss", null);
+                    //ファイルの作成日時を変更
+                    //System.IO.File.SetCreationTime(imgFile, dt);
+                    exiftime = dt;
                 }
             }
             bmp.Dispose();
 
-            return cname;
+            
+        }
+        private void xLog(String s)
+        {
+            //ログ出力
+            DateTime dt = DateTime.Now;
+            textBox1.AppendText(dt.ToString() + " " + s + "\n");
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(@"log.txt", true, System.Text.Encoding.GetEncoding("shift_jis"));
+            sw.Write(dt.ToString() + s + "\r\n");
+            sw.Close();
         }
 
     }
